2fb913fd10d9d2f879d96751f3fe18a019806764
Thiago Loschi <thiagoloschi@gmail.com>
2017-06-09 23:43:08 -0300

    :punch: Final commit
    Menu implemented, alongside with some smooth updates on the layout and
    events handler improved with jquey and pure JS.

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,7 +1,7 @@
 // Verifica o input de telefone e habilita ou desabilita o botão
 $('#phone').keyup(function() {
     var tel = $(this).val();
-    if(tel.length > 13 && tel.length < 15){
+    if(tel.length > 13 && tel.length <= 15){
         $('#send').prop("disabled", false);
         $('#send').css({"border":"solid 2px #505050"});
         $('#send').css({"color":"#505050"});
@@ -22,6 +22,23 @@ $('#send').click(function(){
 
 // Monitora o evento de redimensionamento da página
 $(window).resize(function() {
+
+    // Define o comportamento do menu quando em telas menores que 768px
+    if($(window).width() < 768){
+
+        $('#menu-itm').css({"display":"none"});
+        $('#anunciar').css({"display":"block"});
+        $('#fav').css({"display":"block"});
+
+    }else{
+        $('#menu-itm').css({"display":"block"});
+        $('#anunciar').css({"display":"none"});
+        $('#fav').css({"display":"none"});
+    }
+});
+
+
+$(document).ready(function() {
 
     // Define o comportamento do menu quando em telas menores que 768px
     if($(window).width() < 768){
